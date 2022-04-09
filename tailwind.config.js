f7b671f89f9a836856671650f79137612c304064
sobujhstu <sobujhd@gmail.com>
2022-04-09 18:01:00 +0600

    initially design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./public/*.{js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
